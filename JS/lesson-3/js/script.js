fc3a051277f8eb8a4cee07cfe6a3fb81b7fbc440
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-11-10 20:26:27 +0600

    update js

--- JS/lesson-3/js/script.js
+++ JS/lesson-3/js/script.js
@@ -10,19 +10,54 @@ $(document).ready(function() {
 
   $('.list').css({
     'padding': '10px',
+    'margin' : '30px',
     'border' : '2px solid #000'
   })
 
-  console.log($('.list li').css('color'))
-  console.log($('.list').height()) //.width()
-  console.log($('.list').innerHeight())
-  console.log($('.list').outerHeight())
-  // console.log($('.list').outerHeight(true)) + pad + mar + border
+  // console.log($('.list li').css('color'))
+  // console.log($('.list').height()) //.width()
+  // console.log($('.list').innerHeight())
+  // console.log($('.list').outerHeight())
+  // // console.log($('.list').outerHeight(true)) + pad + mar + border
 
+  // $('.list').height(200)
+  // console.log($('.list').height())
 
+  // $('.list').css({
+  //   'height': '100px',
+  //   'overflow': 'auto'
+  // })
 
+  // $('.list li').click(function(e) {
+  //   console.log($(this).text())
+  //   console.log(e)
+  // })
 
+  // $('.list li').hover(
+  //   function(){
+  //   $(this).text('Change')
+  //   },
+  //   function(){
+  //     $(this).text('!!!')
+  //   }
+  // )
 
+  $('#form').focusin(function() {
+    $(this).css({
+      'border': '1px solid blue'
+    })
+  })
+
+  $('#form').focusout(function() {
+    $(this).css({
+      'border': 'none'
+    })
+  })
+
+  //change
+  $('input').change(function() {
+    $('.box').text($(this).val())
+  })
 
 
 })
