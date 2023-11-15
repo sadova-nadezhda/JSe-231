e33e654f73bb2486e82dff2e0a0536f3f9df5a9d
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-11-15 20:20:19 +0600

    update JS

--- JS/lesson-4/js/script.js
+++ JS/lesson-4/js/script.js
@@ -2,27 +2,41 @@ $(document).ready(function(){
   // Добавление внутри бокса
 
   // Добавляем в начало box1
-  $('.box1').prepend('<p>1111</p>')
-  $('<p>1111</p>').prependTo('.box1')
+  // $('.box1').prepend('<p>1111</p>')
+  // $('<p>1111</p>').prependTo('.box1')
 
   // Добавляем в конец box1
-  $('.box1').append('<p>2222</p>')
-  $('<p>2222</p>').appendTo('.box1')
+  // $('.box1').append('<p>2222</p>')
+  // $('<p>2222</p>').appendTo('.box1')
 
 
   // Добавление за пределами бокса
 
   // Добавляем перед box1
-  $('.box1').before('<h4>Заголовок</h4>')
-  $('<h4>Заголовок</h4>').insertBefore('.box1')
+  // $('.box1').before('<h4>Заголовок</h4>')
+  // $('<h4>Заголовок</h4>').insertBefore('.box1')
 
   // Добавляем после box1
-  $('.box1').after('<ul>abc</ul>')
-  $('<ul>abc</ul>').insertAfter('.box1')
+  // $('.box1').after('<ul>abc</ul>')
+  // $('<ul>abc</ul>').insertAfter('.box1')
 
   // Создать копию (глубокое копирование)
   // $('.box2').clone().appendTo('.box1')
   // $('.box2').clone().prependTo('.box1')
+
+  // Замена элемента
+  // $('.box1').replaceWith('<h4>Заголовок</h4>')
+  // $('<h4>Заголовок2</h4>').replaceAll('h4')
+
+  // Работаем с оберткой
+
+  // $('.box1').wrap('<div class="wrapper"></div>')
+  // $('.box1').wrapInner('<div class="inner"></div>')
+
+  // $('.box1').wrapAll('<div class="boxes"></div>')
+
+  // $('.txt').unwrap()
+
 
 
 });
