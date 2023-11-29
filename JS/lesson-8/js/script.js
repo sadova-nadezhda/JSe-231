914e8230d47708213c0058eb8d4df1318df89dfe
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-11-29 21:22:06 +0600

    Update script.js

--- JS/lesson-8/js/script.js
+++ JS/lesson-8/js/script.js
@@ -36,7 +36,7 @@
 //   return new Promise((resolve, reject)=>{
 //     setTimeout(()=> {
 //       data.modified = true
-//       reject(data)
+//       resolve(data)
 //     }, 2000)
 //   })
 // })
@@ -51,9 +51,26 @@
 // .catch(err => console.error('Error', err))
 // .finally(()=> console.log('Finally'))
 
+
+// const delay = (ms, n) => {
+//   return new Promise(r => setTimeout(()=> r(n), ms))
+// }
+
+// delay(2000).then(()=> console.log('2 sec'))
+// delay(3000).then(()=> console.log('3 sec'))
+
+// Promise.all([delay(2000, 1), delay(5000, 2)]).then(([d1, d2])=> {
+//   console.log(d1)
+//   console.log(d2)
+// })
+
+// Promise.race([delay(2000, 1), delay(5000, 2)]).then((val)=> {
+//   console.log(val)
+// })
+
 // Fetch
 
-let requestURL = 'https://jsonplaceholder.typicode.com/users'
+// let requestURL = 'https://jsonplaceholder.typicode.com/users'
 
 // function sendRequest(method, url, body = null) {
 //   return fetch(url).then(response => {
@@ -66,24 +83,62 @@ let requestURL = 'https://jsonplaceholder.typicode.com/users'
 // .catch( err => console.log(err))
 
 
-function sendRequest(method, url, body = null) {
-  const headers = {
-    'Content-Type': 'application/json; charset=utf-8'
+// function sendRequest(method, url, body = null) {
+//   const headers = {
+//     'Content-Type': 'application/json; charset=utf-8'
+//   }
+//   return fetch(url, {
+//     method: method,
+//     body: JSON.stringify(body),
+//     headers: headers
+//   }).then(response => {
+//     return response.json()
+//   })
+// }
+
+// const body = {
+//   name: 'user',
+//   age: 26
+// }
+
+// sendRequest('POST', requestURL, body)
+// .then( data => console.log(data))
+// .catch( err => console.log(err))
+
+
+// Async/Await
+
+
+const delay = (ms, n) => {
+  return new Promise(r => setTimeout(()=> r(n), ms))
+}
+
+// delay(2000).then(()=> console.log('2 sec'))
+// delay(3000).then(()=> console.log('3 sec'))
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+// function fetchTodos() {
+//   console.log('Fetch todo started...')
+//   return delay(2000).then(()=> {
+//     return fetch(url)
+//   }).then(response => response.json())
+// }
+
+// fetchTodos()
+// .then(data => console.log('Data:', data))
+// .catch(err => console.error(err))
+
+async function fetchAsyncTodos() {
+  console.log('Fetch todo started...')
+  await delay(2000)
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log('Data:', data)
+  if(data) {
+    console.log('end')
   }
-  return fetch(url, {
-    method: method,
-    body: JSON.stringify(body),
-    headers: headers
-  }).then(response => {
-    return response.json()
-  })
 }
 
-const body = {
-  name: 'user',
-  age: 26
-}
+fetchAsyncTodos()
 
-sendRequest('POST', requestURL, body)
-.then( data => console.log(data))
-.catch( err => console.log(err))
