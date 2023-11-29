2a72d95fc0e9fa10ab18d41ae1120ed0ea70308b
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-11-29 20:18:53 +0600

    Update script.js

--- JS/lesson-8/js/script.js
+++ JS/lesson-8/js/script.js
@@ -1,5 +1,6 @@
+// Callback
 
-console.log('Request data...')
+// console.log('Request data...')
 
 // setTimeout(()=> {
 //   console.log('Preparing data...')
@@ -16,35 +17,73 @@ console.log('Request data...')
 
 // }, 2000)
 
+// Promise
 
-let p = new Promise((resolve, reject)=> {
-  setTimeout(()=> {
-    console.log('Preparing data...')
+// let p = new Promise((resolve, reject)=> {
+//   setTimeout(()=> {
+//     console.log('Preparing data...')
 
-    const backendData = {
-      server: 'aws',
-      port: 2000,
-      status: 'working'
-    }
-    resolve(backendData)
-  }, 2000)
-})
+//     const backendData = {
+//       server: 'aws',
+//       port: 2000,
+//       status: 'working'
+//     }
+//     resolve(backendData)
+//   }, 2000)
+// })
 
-p.then( data => {
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=> {
-      data.modified = true
-      reject(data)
-    }, 2000)
+// p.then( data => {
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(()=> {
+//       data.modified = true
+//       reject(data)
+//     }, 2000)
+//   })
+// })
+// .then(clientData => {
+//     console.log('Data received', clientData)
+//     clientData.fromPromise = true
+//     return clientData
+//   })
+// .then (Totaldata => {
+//     console.log('Totaldata', Totaldata)
+//   })
+// .catch(err => console.error('Error', err))
+// .finally(()=> console.log('Finally'))
+
+// Fetch
+
+let requestURL = 'https://jsonplaceholder.typicode.com/users'
+
+// function sendRequest(method, url, body = null) {
+//   return fetch(url).then(response => {
+//     return response.json()
+//   })
+// }
+
+// sendRequest('GET', requestURL)
+// .then( data => console.log(data))
+// .catch( err => console.log(err))
+
+
+function sendRequest(method, url, body = null) {
+  const headers = {
+    'Content-Type': 'application/json; charset=utf-8'
+  }
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers
+  }).then(response => {
+    return response.json()
   })
-})
-.then(clientData => {
-    console.log('Data received', clientData)
-    clientData.fromPromise = true
-    return clientData
-  })
-.then (Totaldata => {
-    console.log('Totaldata', Totaldata)
-  })
-.catch(err => console.error('Error', err))
-.finally(()=> console.log('Finally'))
+}
+
+const body = {
+  name: 'user',
+  age: 26
+}
+
+sendRequest('POST', requestURL, body)
+.then( data => console.log(data))
+.catch( err => console.log(err))
